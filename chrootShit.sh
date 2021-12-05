54b58b1488013dda0ff7ecfd280adf3a6d99ffa5
SMproductive <SMproductive@tutanota.de>
2021-12-05 18:15:21 +0100

    finished

--- chrootShit.sh
+++ chrootShit.sh
@@ -61,7 +61,7 @@ Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
 #gue and programs
 cd /home/"$username"
 pacman -Sy archlinux-keyring --noconfirm
-pacman -Sy alsa pulseaudio pavucontrol git sudo alacritty zsh grml-zsh-config go firefox vim chromium pcmanfm-gtk3 slock feh ttf-font-awesome ttf-opensans adobe-source-code-pro-fonts yarn dmenu papirus-icon-theme --noconfirm
+pacman -Sy man alsa pulseaudio pavucontrol git sudo alacritty zsh grml-zsh-config go firefox vim chromium pcmanfm-gtk3 slock feh ttf-font-awesome ttf-opensans adobe-source-code-pro-fonts yarn dmenu papirus-icon-theme --noconfirm
 pacman -S xdm-openrc xorg-server xf86-video-intel xorg-xbacklight --noconfirm
 
 mkdir suckless
@@ -94,20 +94,23 @@ cp -r configurations/gtk-3.0 .config/gtk-3.0
 ln -P configurations/zshrc .zshrc
 	#vim
 ln -P configurations/vimrc .vimrc
-	#iptables
-iptables-restore configurations/iptables.rules
 	#vimplug
 curl -fLo /home/"$username"/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	#tapping of touchpad
-echo "
+echo '
 Section "InputClass"
 	Identifier "touchpad"
 	Driver "libinput"
 	MatchIsTouchpad "on"
 	Option "Tapping" "on"
-EndSection" > /etc/X11/xorg.conf.d/30-touchpad.conf
+EndSection' > /etc/X11/xorg.conf.d/30-touchpad.conf
+	#iptables
+iptables-restore configurations/iptables.rules
+/etc/init.d/iptables save
 
 chown -hR "$username":"$username" .
+chsh -s /bin/zsh $username
 rc-update add xdm
+rc-update add iwd
 exit
